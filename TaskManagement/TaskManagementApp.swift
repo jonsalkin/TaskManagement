14e15c79a56263e582e7ebec7f8c7d037482d195
jonsalkin <jon.salkin@gmail.com>
2023-07-15 11:13:27 -0400

    Initial Commit

---
+++ TaskManagement/TaskManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementApp.swift
+//  TaskManagement
+//
+//  Created by Jon Salkin on 7/15/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
